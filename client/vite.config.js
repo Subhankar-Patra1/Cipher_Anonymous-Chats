757f1cef1d77950045be5046079dd79d722bb46d
Subhankar-Patra1 <subhankarpatra258@gmail.com>
2026-01-29 16:45:54 +0530

    Configure Vite to suppress console logs in production

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
+  },
   plugins: [
     react(),
     nodePolyfills({
@@ -52,7 +55,7 @@ export default defineConfig({
   define: {
     global: 'window',
   },
-})
+}))
 
 
 
